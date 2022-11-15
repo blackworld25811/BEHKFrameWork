1ddc0fad03a0275fe5c9528bd4ceed2ca9da932b
blackworld25811 <happy_hw@163.com>
2022-11-15 17:03:16 +0800

    change root dirtectory

---
+++ Runtime/BEHKFrameWork/Binding/Components/TextBinding.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography;
+using BEHKFrameWork.Message;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BEHKFrameWork.Binding.Component
+{
+    public class TextBinding : MonoBehaviour
+    {
+        public string Key;
+
+        public Text Text;
+
+
+        private void Start()
+        {
+            BindingAttribute bindingAttribute = BindingListenerData.Instance.GetBindingAttribute(Key);
+
+            if (bindingAttribute != null)
+            {
+                BindingComponentValue<object> bindingComponentValue = new BindingComponentValue<object>(Text.text);
+                bindingComponentValue.OnValueChanged = ChangeText;
+                bindingAttribute.BindingComponentValueList.Add(bindingComponentValue);
+            }
+        }
+
+        private void ChangeText(object text)
+        {
+            Text.text = (string)text;
+        }
+    }
+}
